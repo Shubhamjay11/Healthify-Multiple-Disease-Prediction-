6cb7fa1ba5307c079ba060fcc2633b0e56164bb5
Shubham Jaybhaye <116708200+Shubhamjay11@users.noreply.github.com>
2023-01-19 17:38:59 +0530

    Add files via upload

---
+++ pages/01_🩺_Disease_Diagnosis.py
@@ -0,0 +1,263 @@
+import json
+
+import config as cfg
+import joblib
+import numpy as np
+import pandas as pd
+import streamlit as st
+from config import doctor_search
+
+# ------------------------------- Disease Prediction utils -------------------------------------------------
+
+
+def get_disease(val):
+    for key, value in cfg.disease_encode_dict.items():
+        if val == value:
+            return key
+
+
+def predict_disease(inp_symptoms):
+    test_lst = np.zeros(len(cfg.symptoms))
+    for k in range(0, len(cfg.symptoms)):
+        for s in inp_symptoms:
+            if s == cfg.symptoms[k]:
+                test_lst[k] = 1
+
+    model = joblib.load(cfg.TRAINED_MODEL_RF)
+    prediction = model.predict([test_lst])
+    prediction = get_disease(prediction[0])
+    return prediction
+
+
+# ------------------------------- Disease information display details utils ---------------------------------------
+
+
+def split_by_pipe(value):
+    """
+    Splits the given text by commas and print each on new line
+    """
+    value_lst = value.split("|")
+
+    return value_lst
+
+
+def med_search(medicine):
+    """
+    Search for a medicine on drugs.com
+    """
+    split_med = medicine.split(" ")
+    if len(split_med) > 1:
+        medicine = "+".join(split_med)
+        url = f"https://www.drugs.com/search.php?searchterm={medicine}"
+
+    url = f"https://www.drugs.com/search.php?searchterm={medicine}"
+
+    return url
+
+
+def get_precautions(disease):
+    """
+    Returns a list of precautions of the specified disease
+    """
+    prec = pd.read_csv(cfg.PRECAUTIONS_DATA)
+
+    precautions = prec.loc[prec["Disease"] == disease].values.tolist()[0][1:]
+
+    return precautions
+
+
+def fetch_disease_info(disease=None):
+    """
+    Will fetch all the information of a particular disease
+    """
+    with open(cfg.DISEASE_DATA) as f:
+        disease_data = json.load(f)
+
+    common_name = disease_data[disease]["common name"]
+    gen_overview = disease_data[disease]["Disease overview"][0].get("general_overview")
+    frequency = disease_data[disease]["Disease overview"][1].get("frequency")
+    gen_info = disease_data[disease]["Disease overview"][2].get("general_info")
+    causes = disease_data[disease]["Disease overview"][2].get("cause")
+
+    try:
+        types = disease_data[disease]["types"]
+    except:
+        pass
+
+    if types:
+        disease_type = split_by_pipe(types)
+    else:
+        disease_type = None
+
+    gen_info_lst = split_by_pipe(gen_info)
+    symptoms = disease_data[disease]["symptoms"].replace("|", ", ")
+    causes = split_by_pipe(causes)
+    precautions = get_precautions(disease)
+    treatment = split_by_pipe(disease_data[disease]["treatment"])
+    medications = split_by_pipe(disease_data[disease]["medications"])
+    specialists = split_by_pipe(disease_data[disease]["specialists"])
+    more_info_link = disease_data[disease]["more info"]
+    return (
+        common_name,
+        gen_overview,
+        frequency,
+        gen_info_lst,
+        symptoms,
+        causes,
+        precautions,
+        treatment,
+        medications,
+        specialists,
+        disease_type,
+        more_info_link,
+    )
+
+
+# def print_list(var, seperator, func_links)
+
+# ========================================== Main app for streamlit page call =================================================
+
+
+def disease_app():
+    st.set_page_config(
+        page_title="Healthify - Disease Diagnosis",
+        page_icon="🏥",
+    )
+    st.markdown(
+        f"<h1 style='text-align: center; color: black;'>Disease Diagnosis to Treatment</h1>",
+        unsafe_allow_html=True,
+    )
+
+    st.markdown(
+        '<p style="font-size:22px; text-align: center; color: black;">Select the symptoms you are suffering from and the algorithm will predict the disease you are suffering from along with precautions, personalized medicine recommendations, specialists and much more.</p>',
+        unsafe_allow_html=True,
+    )
+
+    st.markdown("***")
+
+    inp_symptoms = st.multiselect("Select the symptoms: ", cfg.symptoms)
+
+    submit = st.button("Predict")
+    if len(inp_symptoms) >= 4:
+        if submit:
+
+            prediction = predict_disease(inp_symptoms)
+            st.markdown(
+                f"<h2 style='text-align: center; color: red;font-weight:bold;'>You are suffering from {prediction}</h2>",
+                unsafe_allow_html=True,
+            )
+            st.write("")
+            st.warning(
+                "Don't panic. Read all the precautions, treatments and consult your nearest specialists if the problem worsens."
+            )
+
+            (
+                common_name,
+                gen_overview,
+                frequency,
+                gen_info_lst,
+                symptoms,
+                causes,
+                precautions,
+                treatment,
+                medications,
+                specialists,
+                disease_type,
+                more_info_link,
+            ) = fetch_disease_info(prediction)
+
+            # ------------------------- General information---------------------------------------------------
+            st.markdown("---")
+            st.subheader("Disease Overview 🧐")
+            st.markdown(f"**Common name** : {common_name}")
+            st.markdown(f"**Description**: {gen_overview}")
+            st.markdown(f"**Frequency 📈**: {frequency}")
+            st.markdown("---")
+            # ------------------------------------------------------------------------------------------------
+
+            # ------------------------- General medical information ------------------------------------------
+
+            # st.markdown("---")
+            st.markdown(f"🏥  {gen_info_lst[0]}")
+            st.markdown(f"💊  {gen_info_lst[1]}")
+            st.markdown(f"🔬  {gen_info_lst[2]}")
+            st.markdown(f"🕒  {gen_info_lst[3]}")
+            st.markdown("---")
+            # ------------------------------------------------------------------------------------------------
+
+            # ------------------------- Causes ---------------------------------------------------------------
+            # st.markdown("---")
+            st.subheader("Causes 📝")
+            for c in causes:
+                st.markdown(f"👉 {c}")
+            st.markdown("---")
+            # -------------------------------------------------------------------------------------------------
+
+            # ------------------------- Types (if exists) -----------------------------------------------------
+            # st.markdown("---")
+            if disease_type is not None:
+                st.subheader("Types 👇")
+                for t in disease_type:
+                    st.markdown(f"- {t}")
+                st.markdown("---")
+            # ---------------------------------------------------------------------------------------------------
+
+            # ------------------------- Symptoms ----------------------------------------------------------------
+            # st.markdown("---")
+            st.subheader("Symptoms 😷")
+            st.markdown(f"{symptoms}.")
+            st.markdown("---")
+            # ----------------------------------------------------------------------------------------------------
+
+            # ----------------------------------Precautions (from csv data)---------------------------------------
+            # st.markdown("---")
+            st.subheader("Precautions 🩹")
+            for prec in precautions:
+                st.markdown(f"📌  {prec}")
+            st.markdown("---")
+            # ----------------------------------------------------------------------------------------------------
+
+            # -------------------------Professional Treatment-----------------------------------------------------
+            # st.markdown("---")
+            # Treatment
+            st.subheader("Professional Treatment 🩺")
+            for t in treatment:
+                st.markdown(f"- {t}")
+            st.markdown("---")
+            st.markdown("---")
+            # ----------------------------------------------------------------------------------------------------
+
+            # ---------------------------- Medications -----------------------------------------------------------
+            # st.markdown("---")
+            st.subheader("Medications 💊")
+            st.write(
+                "Click on the medicine name to get more info (dosage, side-effects,etc)"
+            )
+            for m in medications:
+                meds = f"- [{m}]({med_search(m)})"
+                st.markdown(meds, unsafe_allow_html=True)
+            st.markdown("---")
+            # -----------------------------------------------------------------------------------------------------
+
+            # ----------------------------------Specialists--------------------------------------------------------
+            # st.markdown("---")
+            st.subheader("Specialists 👨‍⚕")
+            st.write(
+                "Click on the specialist's name to find out the nearest specialist to you ..."
+            )
+            for s in specialists:
+                doctor = f"- [{s}]({doctor_search(s)})"
+                st.markdown(doctor, unsafe_allow_html=True)
+            st.markdown("---")
+            # -----------------------------------------------------------------------------------------------------
+
+            # More
+            st.subheader(
+                f"To read more about this disease visit this [link]({more_info_link})"
+            )
+    else:
+        st.error("Please enter atleast 4 symptoms")
+
+
+if __name__ == "__main__":
+    disease_app()
